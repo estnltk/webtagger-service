08b1842d50bf645bdb2cfb15392c827c78c5f035
pault <pault@ut.ee>
2020-10-11 21:50:52 +0300

    update morph_analysis_web_pipeline

--- webtagger_service/services/tag_layer.py
+++ webtagger_service/services/tag_layer.py
@@ -1,7 +1,14 @@
-from estnltk.converters import json_to_text, text_to_json
+from estnltk import Text
+from estnltk.converters import text_to_json, json_to_layers
 
 
-def tag_layer(layer_names, data):
-    text = json_to_text(data)
-    text.tag_layer(layer_names.split(','))
+def tag_layer(data):
+    text = Text(data['text'])
+    text.meta = data['meta']
+    layers = json_to_layers(text, data['layers'])
+
+    for layer in Text.topological_sort(layers):
+        text.add_layer(layer)
+
+    text.tag_layer(layer_names=data['parameters']['layer_names'])
     return text_to_json(text)
